c4cbd0404dd6f0e64b77dab08c467437405f48b8
zhuqijiaying <2975049984@qq.com>
2022-11-08 23:12:09 +0800

    学习

---
+++ src/comFive_常用类/Math/ds.java
@@ -0,0 +1,13 @@
+package comFive_常用类.Math;
+
+/**
+ * @author zq
+ */
+public class ds {
+    public static void main(String[] args) {
+        for(int i = 0 ;i<=100;i++){
+            System.out.println((int)(Math.random()*100)+1);
+        }
+
+    }
+}
