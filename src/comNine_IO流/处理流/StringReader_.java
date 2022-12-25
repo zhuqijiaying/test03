11cdad7dee8dbab406b221b32726c85e302a8124
zhuqijiaying <2975049984@qq.com>
2022-12-25 20:19:59 +0800

    学习

---
+++ src/comNine_IO流/处理流/StringReader_.java
@@ -0,0 +1,11 @@
+package comNine_IO流.处理流;
+
+/**
+ * @author zq
+ */
+//节点流
+public class StringReader_ extends Reader_{
+    public void readString(){
+        System.out.println("读取字符串");
+    }
+}
