4795495c385a7a741272967070783658b1761548
zhuqijiaying <2975049984@qq.com>
2022-12-28 22:15:17 +0800

    学习

---
+++ src/comTen_reflection/class_/Car.java
@@ -0,0 +1,41 @@
+package comTen_reflection.class_;
+
+/**
+ * @author zq
+ */
+public class Car {
+    public String name="宝马";
+    public double price=244324;
+
+    public Car() {
+    }
+
+    public Car(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
