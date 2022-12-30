eeb8259624dc2b0d4e3bf3743ff6aa060982b5b7
zhuqijiaying <2975049984@qq.com>
2022-12-30 21:00:45 +0800

    学习

---
+++ src/comTen_reflection/reflectionQuestion1/Cat.java
@@ -0,0 +1,16 @@
+package comTen_reflection.reflectionQuestion1;
+
+/**
+ * @author zq
+ */
+public class Cat {
+    private String name = "招财猫";
+    public int age = 10;
+    public Cat(){}
+    public Cat(String name){
+        this.name = name;
+    }
+    public void hi(){
+        //System.out.println("hi"+name);
+    }
+}
